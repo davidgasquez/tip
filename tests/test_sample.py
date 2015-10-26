ca01428eabee6c9e3ce91dce29be1ace8b530381
David Gasquez <davidgasquez@gmail.com>
2015-10-26 17:47:53 +0100

    Add dummy code to test

---
+++ tests/test_sample.py
@@ -0,0 +1,8 @@
+import unittest
+from tip.algorithms.dummy import dummy_add
+
+
+class TestDummyAdd(unittest.TestCase):
+    def test_lcm(self):
+        r = dummy_add(2, 2)
+        self.assertEqual(r, 4)
