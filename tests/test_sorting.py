4d24478db8462ae64f2727ecd6910e9a7f667efd
David Gasquez <davidgasquez@gmail.com>
2015-11-07 16:08:49 +0100

    Update tests

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -2,9 +2,24 @@ import unittest
 from tip.algorithms.sorting.mergesort import mergesort
 
 
-class TestSorting(unittest.TestCase):
+class TestMergesort(unittest.TestCase):
 
-    def test_mergesort(self):
+    def test_mergesort_basic(self):
         unsorted_list = [5, 3, 7, 8, 9, 3]
+        sorted_list = mergesort(unsorted_list)
+        self.assertEqual(sorted_list, sorted(unsorted_list))
+
+    def test_mergesort_trivial(self):
+        unsorted_list = [1]
+        sorted_list = mergesort(unsorted_list)
+        self.assertEqual(sorted_list, sorted(unsorted_list))
+
+    def test_mergesort_with_duplicates(self):
+        unsorted_list = [1, 1, 1, 1]
+        sorted_list = mergesort(unsorted_list)
+        self.assertEqual(sorted_list, sorted(unsorted_list))
+
+    def test_mergesort_with_empty_list(self):
+        unsorted_list = []
         sorted_list = mergesort(unsorted_list)
         self.assertEqual(sorted_list, sorted(unsorted_list))
