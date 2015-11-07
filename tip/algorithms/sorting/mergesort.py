a4a922d7c3ad1a9410752f984d5bd15f99e10cf6
David Gasquez <davidgasquez@gmail.com>
2015-11-07 14:06:17 +0100

    :memo: Add documentation

--- tip/algorithms/sorting/mergesort.py
+++ tip/algorithms/sorting/mergesort.py
@@ -1,14 +1,33 @@
-def merge(a, b):
-    if len(a) * len(b) == 0:
-        return a + b
+""" Merge Sort
+The Merge Sort is a recursive sort of order n*log(n).
 
-    v = (a[0] < b[0] and a or b).pop(0)
-    return [v] + merge(a, b)
+It is notable for having a worst case and average complexity of O(n*log(n)),
+and a best case complexity of O(n) (for pre-sorted input).
+
+The basic idea is to split the collection into smaller groups by halving it
+until the groups only have one element or no elements (which are both entirely
+sorted groups).Then merge the groups back together so that their elements are
+in order.
+
+This is how the algorithm gets its "divide and conquer" description.
+"""
 
 
-def mergesort(list):
-    if len(list) < 2:
-        return list
+def merge(left, right):
+    """Merge two list keeping the elements in order."""
+    if len(left) * len(right) == 0:
+        return left + right
 
-    m = len(list) / 2
-    return merge(mergesort(list[:int(m)]), mergesort(list[int(m):]))
+    merge_list = (left[0] < right[0] and left or right).pop(0)
+    return [merge_list] + merge(left, right)
+
+
+def mergesort(unsorted_list):
+    """Merge Sort function."""
+    if len(unsorted_list) < 2:
+        return unsorted_list
+
+    mid = len(unsorted_list) / 2
+    left_part = mergesort(unsorted_list[:int(mid)])
+    right_part = mergesort(unsorted_list[int(mid):])
+    return merge(left_part, right_part)
